64adca8655e5cdc7b5a6c893a0595d95abec7887
jcaswell <jakecaswell88@gmail.com>
2021-10-17 14:47:03 -0400

    added html template and made html file through js

--- app.js
+++ app.js
@@ -1,15 +1,12 @@
+const fs = require('fs');
+const generatePage = require('./src/page-template.js');
+
 const profileDataArgs = process.argv.slice(2, process.argv.length);
 
-const printProfileData = profileDataArr => {
-    // This... 
-    for (let i = 0; i < profileDataArr.length; i++) {
-        console.log(profileDataArr[i]);
-    }
-    
-    console.log('================');
+const [name, github] = profileDataArgs;
 
-    // is the same as this 
-    profileDataArr.forEach((profileItem) => console.log(profileItem));
-}
+fs.writeFile('index.html', generatePage(name, github), err => {
+    if (err) throw err;
 
-printProfileData(profileDataArgs);
+    console.log('Portfolio complete! Check out index.html to see the output!');
+});
